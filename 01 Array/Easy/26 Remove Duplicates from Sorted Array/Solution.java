c4473bf22a154d6366cb7912c3746f1346d95a13
sathyavardh <ksathyavardhanec@gmail.com>
2025-07-30 21:05:19 +0530

    first commit

---
+++ 01 Array/Easy/26 Remove Duplicates from Sorted Array/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int x=0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]>nums[x]){
+                x+=1;
+                nums[x]=nums[i];
+            }
+        }
+        return x+1;
+        
+    }
+}
