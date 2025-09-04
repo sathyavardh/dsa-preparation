d3574261b47af057869dcfd41b237bdffd53ddf5
Sathyavardhan K <94102256+sathyavardh@users.noreply.github.com>
2025-09-04 08:51:40 +0530

    Update sept4

--- 04 Stack & Queue/Medium/150 Evaluate Reverse Polish Notation/Solution.java
+++ 04 Stack & Queue/Medium/150 Evaluate Reverse Polish Notation/Solution.java
@@ -3,6 +3,7 @@
 class Solution {
     public int evalRPN(String[] tokens) {
         Stack<String> stack = new Stack<>();
+
         for(int i=0; i<tokens.length; i++){
             String token = tokens[i];
             if(!token.equals("+") && !token.equals("-") && !token.equals("*") && !token.equals("/")){
