ee3f9c2dc696e5bfa4f737923bc62f6124607e15
sathyavardh <ksathyavardhanec@gmail.com>
2025-09-23 22:31:27 +0530

    fixed day56

--- 06 TwoPointers & Sliding Windows/Easy/28 Find the Index of the First Occurrence in a String/Solution.java
+++ 06 TwoPointers & Sliding Windows/Easy/28 Find the Index of the First Occurrence in a String/Solution.java
@@ -1,3 +1,5 @@
+// Approach - 1 Brute Force (Sliding Window)
+
 class Solution {
     public int strStr(String haystack, String needle) {
         int n = haystack.length();
@@ -16,5 +18,54 @@ class Solution {
         }
         return -1;
         
+    }
+}
+
+
+// Approach - 2 KMP Algo
+
+class Solution {
+    public int strStr(String haystack, String needle) {
+        int n = haystack.length();
+        int m = needle.length();
+
+        int[] lps = new int[m];
+        lps[0]=0;
+        int i=0,j=1;
+
+        //Building LPS Arr
+        while(j<m){
+            if(needle.charAt(i)==needle.charAt(j)){
+                lps[j]=i+1;
+                ++i;++j;
+            }else{
+                if(i==0){
+                    lps[j]=0;
+                    ++j;
+                }else{
+                    i=lps[i-1];
+                }
+            }
+        }
+
+        //Check for the String needle in haystack using KMP
+        i=j=0;
+
+        while(i<n){
+            if(haystack.charAt(i)==needle.charAt(j)){
+                ++i;++j;
+            }else{
+                if(j==0){
+                    ++i;
+                }else{
+                    j=lps[j-1];
+                }
+            }
+
+            if(j==m){
+                return i-m;
+            }
+        }
+        return -1; //no match
     }
 }
